bd65166e97e416341cd1d8aaa2f97d6327e0edff
saessei <danielleanne.poral-22@cpu.edu.ph>
2026-03-13 12:51:40 +0800

    feat: add supabaseClient.ts

---
+++ src/config/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+
+const supabaseUrl: string = process.env.SUPABASE_URL as string
+const supabaseKey: string = process.env.SUPABASE_KEY as string
+
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
