5a2b5f20fab0e4f67e47526e6a65d3ce2fb40f25
saessei <danielleanne.poral-22@cpu.edu.ph>
2026-03-13 15:11:19 +0800

    feat: add routing and auth components

---
+++ src/components/Signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+export const Signup = () => {
+  return (
+    <div>
+      <form>
+        <h2> Sign up </h2>
+      </form>
+    </div>
+  )
+}
