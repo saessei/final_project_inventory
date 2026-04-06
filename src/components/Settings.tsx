3e8c6152101abd48c63e7e94c488df8869c2aafa
saessei <danielleanne.poral-22@cpu.edu.ph>
2026-04-06 16:06:49 +0800

    feat: add handle update name function

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,12 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { UserAuth } from "../context/AuthContext";
+import { profileService } from "../services/profileService";
 
 export const Settings = () => {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const { user } = UserAuth();
-    
-  return (
-    <div>Settings</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { session } = UserAuth();
+  const user = session?.user;
+
+  useEffect(() => {
+    if (session?.user?.id) {
+      profileService
+        .getProfile(session.user.id)
+        .then((data) => {
+          setName(data.full_name || "");
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.error(err);
+          setLoading(false);
+        });
+    }
+  }, [session]);
+
+  const handleUpdateName = async () => {
+    try {
+      await profileService.updateName(user.id, name);
+      console.log("Name updated successfully!");
+    } catch (err) {
+      console.log("Update failed");
+    }
+  };
+  
+  return <div>Settings</div>;
+};
